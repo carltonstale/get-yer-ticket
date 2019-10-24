29f765466ed1118cd0ef4600fe9098885ec7fcfd
carltonstale <dougkingston216@gmail.com>
2019-10-23 20:31:16 -0500

    data is seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Customer.destroy_all
+
 User.create(name: "Cranky", password: "tesTing123", password_confirmation: "tesTing123", email: "test@test.com")
 User.create(name: "Crabby", password: "tesTing123", password_confirmation: "tesTing123", email: "test2@test2.com")
+User.create(name: "Boss", password: "tesTing123", password_confirmation: "tesTing123", email: "test3@test3.com", admin: true)
 
-Customer.create(name: "Fred", email: "fred@fred.com", phone: "555-5555")
-Customer.create(name: "George", email: "george@george.com", phone: "555-5555")
+Customer.create(name: "Fred", email: "fred@fred.com", phone_number: "555-5555")
+Customer.create(name: "George", email: "george@george.com", phone_number: "555-5555")
 
