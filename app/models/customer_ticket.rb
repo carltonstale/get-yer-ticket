df79b8af351b2517975be71f329a1f2ce2eba34b
carltonstale <dougkingston216@gmail.com>
2019-10-26 16:52:25 -0500

    removes customer_ticket model and controller

--- app/models/customer_ticket.rb
+++
@@ -1,6 +0,0 @@
-class CustomerTicket < ApplicationRecord
-    belongs_to :ticket
-    belongs_to :customer
-
-    # this will allow customer to see their own tickets, in list and individual format, but with no admin notes 
-end
